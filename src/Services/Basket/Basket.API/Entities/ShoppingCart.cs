c88447694121f1c070caebe1b4f8b60582747bd9
Santhosh Venugopal <santhoshece60@gmail.com>
2022-04-01 15:56:38 +0530

    BASKET.API till Repository Pattern

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        // We will use this method since we are just getting data from it, not setting any data
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var items in Items)
+                {
+                    totalPrice += items.Price * items.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
+
+
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        // We are using IDistributedCache cache object as a Redis Cache
+        // We have configured the Redis cache as a distributed cache in startup class
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+            // It will deserialize the basket to the ShoppingCart Structure
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+
+        }
+    // In Redis DB values are stored in key value pair. So if we did any changes to value the entire values w.r.t key 
+    // will be updated. Thats why we dont need sepeare CREATE,UPDATE,PUT requests
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+    }
+}
